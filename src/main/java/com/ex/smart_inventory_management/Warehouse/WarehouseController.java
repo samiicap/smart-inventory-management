dac0eaf75970018d8e1652a9f55b9166aba7d28d
pnooru <p-noorul-sameera.p-noorul-sameera@capgemini.com>
2026-06-11 12:12:59 +0530

    created Warehouse Module

---
+++ src/main/java/com/ex/smart_inventory_management/Warehouse/WarehouseController.java
@@ -0,0 +1,4 @@
+package com.ex.smart_inventory_management.Warehouse;
+
+public class WarehouseController {
+}
--- src/main/java/com/ex/smart_inventory_management/stock/StockController.java
+++ src/main/java/com/ex/smart_inventory_management/stock/StockController.java
@@ -1,4 +1,21 @@
 package com.ex.smart_inventory_management.stock;
+import java.util.List;
+
+
+import org.springframework.web.bind.annotation.*;
+
+
+import lombok.RequiredArgsConstructor;
+
+
+import com.example.inventory.entity.Stock;
+import com.example.inventory.service.StockService;
+
+
+
+@RestController
+@RequestMapping("/stock")
+@RequiredArgsConstructor
 
 public class StockController {
 }
