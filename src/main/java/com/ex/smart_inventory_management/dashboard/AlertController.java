c300e663eff0bc6f3c90040a9fe331946c55300e
SRUSHK <srushti.k@capgemini.com>
2026-06-14 23:22:46 +0530

    Implement AlertController to retrieve alerts

--- src/main/java/com/ex/smart_inventory_management/dashboard/AlertController.java
+++ src/main/java/com/ex/smart_inventory_management/dashboard/AlertController.java
@@ -1,4 +1,22 @@
 package com.ex.smart_inventory_management.dashboard;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alerts")
+@RequiredArgsConstructor
 public class AlertController {
+
+	private final AlertService service;
+
+	@GetMapping
+	public List<AlertResponseDto> getAlerts(){
+		return service.getAlerts();
+	}
+
 }
