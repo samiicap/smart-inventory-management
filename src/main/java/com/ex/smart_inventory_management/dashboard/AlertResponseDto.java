739896d77f32f1ea91dd43c6bff037cf0779be4e
SRUSHK <srushti.k@capgemini.com>
2026-06-14 23:23:18 +0530

    Add Lombok @Data annotation to AlertResponseDto

--- src/main/java/com/ex/smart_inventory_management/dashboard/AlertResponseDto.java
+++ src/main/java/com/ex/smart_inventory_management/dashboard/AlertResponseDto.java
@@ -1,4 +1,14 @@
 package com.ex.smart_inventory_management.dashboard;
 
+import lombok.Data;
+
+@Data
 public class AlertResponseDto {
+
+	private Long productId;
+	private String productName;
+	private int quantity;
+	private int minimumStock;
+	private String message;
+
 }
