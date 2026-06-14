4886acafa838e50d4ae49dc7f4ab11fa00444290
SRUSHK <srushti.k@capgemini.com>
2026-06-14 23:23:50 +0530

    Add getAlerts method to AlertService
    Implement method to retrieve low stock alerts for products.

--- src/main/java/com/ex/smart_inventory_management/dashboard/AlertService.java
+++ src/main/java/com/ex/smart_inventory_management/dashboard/AlertService.java
@@ -1,4 +1,36 @@
 package com.ex.smart_inventory_management.dashboard;
 
+import com.ex.smart_inventory_management.product.ProductDto;
+import com.ex.smart_inventory_management.product.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class AlertService {
+
+	private final ProductRepository productRepository;
+
+	public List<AlertResponseDto> getAlerts(){
+
+		List<ProductDto> products = productRepository.findAll();
+
+		return products.stream()
+				.filter(p -> p.getQuantity() <= p.getMinimumStock())
+				.map(p -> {
+					AlertResponseDto a = new AlertResponseDto();
+					a.setProductId(p.getId());
+					a.setProductName(p.getName());
+					a.setQuantity(p.getQuantity());
+					a.setMinimumStock(p.getMinimumStock());
+					a.setMessage("Product stock is low");
+					return a;
+				})
+				.collect(Collectors.toList());
+
+	}
+
 }
