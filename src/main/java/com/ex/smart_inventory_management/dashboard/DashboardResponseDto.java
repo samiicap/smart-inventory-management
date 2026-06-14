fce245b18d0b51bd91a022d6b34be4983acb5e69
SRUSHK <srushti.k@capgemini.com>
2026-06-14 23:24:45 +0530

    Update DashboardResponseDto.java

--- src/main/java/com/ex/smart_inventory_management/dashboard/DashboardResponseDto.java
+++ src/main/java/com/ex/smart_inventory_management/dashboard/DashboardResponseDto.java
@@ -1,4 +1,14 @@
 package com.ex.smart_inventory_management.dashboard;
 
+import lombok.Data;
+
+@Data
 public class DashboardResponseDto {
+
+	private long totalProducts;
+	private long totalWarehouses;
+	private long totalStock;
+	private long totalAvailableStock;
+	private long lowStockProducts;
+
 }
