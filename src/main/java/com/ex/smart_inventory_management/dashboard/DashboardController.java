f4c32f17d5223c8ea9f6611d1d8b608d17a609fc
SRUSHK <srushti.k@capgemini.com>
2026-06-14 23:24:19 +0530

    Update DashboardController.java

--- src/main/java/com/ex/smart_inventory_management/dashboard/DashboardController.java
+++ src/main/java/com/ex/smart_inventory_management/dashboard/DashboardController.java
@@ -1,4 +1,20 @@
 package com.ex.smart_inventory_management.dashboard;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dashboard")
+@RequiredArgsConstructor
 public class DashboardController {
+
+	private final DashboardService service;
+
+	@GetMapping
+	public DashboardResponseDto getSummary(){
+		return service.getDashboard();
+	}
+
 }
